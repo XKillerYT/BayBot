23c7fa57ddf8a67feaa55076fd09a2c4b97a6112
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-10-03 19:14:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1146,7 +1146,7 @@ client.on('message', function(message) {
 client.on('message',async msg => {
      if(msg.channel.type === "dm") return;
   if(msg.author.bot) return;
-  var p = "-";
+  var p = "?";
   if(msg.content.startsWith(p + "setstats")) {
   if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
   if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
