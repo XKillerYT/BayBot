eabecfc6266e2aa49f5593abe164647b6be42499
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-10-18 18:09:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1460,6 +1460,30 @@ const pubg = [
  }
 });
 
+const AllwaiDiamondCodes = ['383606619671494659' , '383606619671494659'];
+client.on('message', message => {
+var prefix = "?";
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!AllwaiDiamondCodes.includes(message.author.id)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  if (command === "say")  {
+  if(!message.channel.guild) return message.reply('** __This command only for servers⛔__  **');
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
+if (command == "emb")    {
+  if(!message.channel.guild) return message.reply('** __This command only for servers⛔__  **');
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor("RANDOM")
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+
 //code
 
 client.login(process.env.BOT_TOKEN);
