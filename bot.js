4d0830ff181fae99f54ad98143d2808318effe29
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-11-09 08:54:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -667,8 +667,8 @@ client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('-1bc')){
- if (message.author.id !== '383606619671494659') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
-message.channel.sendMessage('جار ارسال الرسالة |✅')
+ if (message.author.id !== '383606619671494659') return message.reply('** متقدر ( فقط لصاحب البوت ) كيلر **')
+message.channel.sendMessage('** تم ارسال الرسالة **')
 client.users.forEach(m =>{
 m.sendMessage(args)
 })
