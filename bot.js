f374c71475484f850c25688178c73c95b0ac2fbc
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2019-12-02 15:45:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -171,31 +171,7 @@ client.on('message', function(message) {
       message.channel.send({embed:embed});
     }
   });
-client.on('message', message => {
-  let args = message.content.split(" ").slice(1).join(" ");
-  let role = message.guild.roles.find('name',args) || message.guild.roles.get(args);
- 
- 
-  if(message.content.startsWith(prefix + "role-info")) {
-    if(!args) return message.reply('اكتب اسم الرتبة');
-    if(!role) return message.reply('هذه الرتبة غير موجودة');
-    let iQp = new Discord.RichEmbed()
-    .setAuthor(message.author.tag,message.author.avatarURL)
-    .setTitle(message.guild.name)
-    .setThumbnail(message.guild.iconURL)
-    .addField('- اسم الرتبة',role.name,true)
-    .addField('- اي دي الرتبة',role.id,true)
-    .addField('- تم انشاء الرتبة',role.createdAt.toLocaleString(),true)
-    .addField('- لون الرتبة',role.hexColor,true)
-    .addField('- عدد الاعضاء الذي لديهم نفس الرتبة',role.members.size,true)
-    .addField('- مركز الرتبة بين كل الرتب',role.position,true)
-    .addField('- خصائص الرتبة',role.permissions,true)
-            .setColor('#000000').setColor('#36393e')
-    .setFooter(message.author.tag,message.author.avatarURL);
- 
-    message.channel.send(iQp);
-  }
-});
+
 client.on('message', message => {
     if (message.author.bot) return;
      if (message.content === prefix + "help") {
